1d09d082a0153dc48f9bc01a7de52bc8e3b32d75
Kelly Hwong <dianhuangkan@gmail.com>
2023-04-12 15:30:53 +0800

    split train val set

---
+++ AzurLaneVoice/split.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : Apr-04-23 17:55
+# @Author  : Kelly Hwong (dianhuangkan@gmail.com)
+
+import os
+import numpy as np
+import pandas as pd
+import random
+from sklearn.model_selection import train_test_split
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
